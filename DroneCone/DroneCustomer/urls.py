cb3b31ecf18ee7a0468c367989ac069a6c484da6
SpencerPotter <a02310145@usu.edu>
2023-10-06 12:29:03 -0600

    added pages

---
+++ DroneCone/DroneCustomer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("orderhistory/", views.orderHistory, name="orderhistory"),
+    path("login/", views.login, name="login"),
+    path("dronemanagment/", views.droneManagment, name="dronemanagmment")
+]
