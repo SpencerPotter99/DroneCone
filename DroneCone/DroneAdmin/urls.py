1c6644f4d155d5f9bd39977fbd7797639c30eafd
Jonah Harmon <jonah.harmon@centeva.com>
2023-10-11 11:03:53 -0600

    Templates are in a basic working order :)

--- DroneCone/DroneAdmin/urls.py
+++ DroneCone/DroneAdmin/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    path('', views.drone_management, name="drone_management"),
-    path('', views.inventory, name="inventory"),
-    path('', views.sales, name="sales")
+    path('', views.index, name='index'),
+    path('drone_management/', views.drone_management, name="drone_management"),
+    path('inventory/', views.inventory, name="inventory"),
+    path('sales/', views.sales, name="sales")
 ]
--- DroneCone/DroneAdmin/views.py
+++ DroneCone/DroneAdmin/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "DroneAdmin/index.html")
+
 def drone_management(request):
-    return render(request, "DroneAdmin.drone_management.html")
+    return render(request, "DroneAdmin/drone_management.html")
 
 def inventory(request):
-    return render(request, "DroneAdmin.inventory.html")
+    return render(request, "DroneAdmin/inventory.html")
 
 
 def sales(request):
-    return render(request, "DroneAdmin.sales.html")
+    return render(request, "DroneAdmin/sales.html")
