4702bba6f7cb359da2128fe23461600d365bfcc4
Jonah Harmon <jonah.harmon@centeva.com>
2023-10-11 11:27:08 -0600

    Got things closer

--- DroneCone/DroneAdmin/views.py
+++ DroneCone/DroneAdmin/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, "DroneAdmin/index.html")
 
+
 def drone_management(request):
     return render(request, "DroneAdmin/drone_management.html")
+
 
 def inventory(request):
     return render(request, "DroneAdmin/inventory.html")
