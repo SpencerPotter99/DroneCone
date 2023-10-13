56c69c8b774a8e64cdf631e9f0b0823afb61c3f5
JHamoni676 <jonahharmon2014@gmail.com>
2023-10-13 15:19:28 -0600

    Updated file names and played with inventory.html

--- DroneCone/DroneAdmin/views.py
+++ DroneCone/DroneAdmin/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, "DroneAdmin/index.html")
+    return render(request, "DroneAdmin/dashboard.html")
 
 
 def drone_management(request):
