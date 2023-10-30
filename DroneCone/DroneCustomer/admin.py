4719ce6ed83137097f1864135b4a57cfd51f3ea3
JHullinger <jhullinger4@gmail.com>
2023-10-30 01:32:15 -0600

    Added basic registration for DroneCustomer models.

--- DroneCone/DroneCustomer/admin.py
+++ DroneCone/DroneCustomer/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Drone)
+admin.site.register(IceCream)
+admin.site.register(Topping)
+admin.site.register(Cone)
+admin.site.register(IceCreamCone)
+admin.site.register(Order)
+admin.site.register(Cart)
