453015bca0e197c83871b91ba96e58b9be21d25e
JHullinger <jhullinger4@gmail.com>
2023-11-07 14:07:38 -0700

    Added Signal to attach a cart to a user.

--- DroneCone/DroneCustomer/apps.py
+++ DroneCone/DroneCustomer/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DronecustomerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DroneCustomer'
+
+    def ready(self):
+        import DroneCustomer.signals  # noqa
