744236e9322fc75e7233aa0a94b263797485f00c
SpencerPotter99 <William.s.potter00@gmail.com>
2023-11-13 09:53:10 -0700

    fixed the order countdown on home page

--- DroneCone/DroneCustomer/views.py
+++ DroneCone/DroneCustomer/views.py
@@ -50,7 +50,8 @@ class OrderListView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user  # Get the currently logged-in user
+        user = self.request.user
+        print(user)
         return Order.objects.filter(user=user)
 
 class UpdateOrderStatusView(APIView):
