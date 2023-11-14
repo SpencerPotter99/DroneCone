ffb5f2d237f13dd7ac0d068b601b3174b7c4fe00
JHamoni676 <jonahharmon2014@gmail.com>
2023-11-14 13:53:24 -0700

    Added a couple tests

---
+++ DroneCone/DroneAdmin/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from DroneCustomer.models import Drone, IceCream, Topping, Cone, IceCreamCone, Order, Cart
+from django.contrib.auth.models import User
+
+class DroneModelTest(TestCase):
+
+    def test_drone_is_available(self):
+        user = User.objects.create(username='testuser')
+        drone = Drone.objects.create(owner=user, name='Test Drone', drone_weight_g=100,
+                                     battery_capacity_mAh=2000, battery_voltage=3.7, battery_level=0.5,
+                                     enabled=True, in_flight=False)
+        self.assertTrue(drone.is_available())
+
+    def test_drone_status_disabled(self):
+        drone = Drone.objects.create(name='Disabled Drone', enabled=False, in_flight=False)
+        self.assertEqual(drone.get_drone_status(), 'Disabled')
