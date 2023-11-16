94fa4c47190d34321385d4913c7d3badd758a4c5
cody.wilson3 <a02313918@usu.edu>
2023-11-16 13:42:57 -0700

    Fixed styling of login, signup, and drone owner signup pages

--- DroneCone/Account/forms.py
+++ DroneCone/Account/forms.py
@@ -8,15 +8,15 @@ from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'custom-email-input'}))
+    email = EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'custom-email-input border border-gray-400 rounded'}))
 
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ("email",)
         widgets = {
-            'username': TextInput(attrs={'class': 'custom-username-input'}),
-            'password1': forms.PasswordInput(attrs={'class': 'custom-password1-input'}),
-            'password2': forms.PasswordInput(attrs={'class': 'custom-password2-input'}),
+            'username': TextInput(attrs={'class': 'custom-username-input border border-gray-400 rounded'}),
+            'password1': forms.PasswordInput(attrs={'class': 'custom-password1-input border border-gray-400 rounded'}),
+            'password2': forms.PasswordInput(attrs={'class': 'custom-password2-input border border-gray-400 rounded'}),
         }
 
         
